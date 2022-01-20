d63ae81867ce2158585585292d73e331771d322e
jbellegarde <bellegarde.j@northeastern.edu>
2022-01-20 11:34:13 -0500

    left a todo

--- app/src/main/java/edu/neu/numad22sp_jovianebellegarde/MainActivity.java
+++ app/src/main/java/edu/neu/numad22sp_jovianebellegarde/MainActivity.java
@@ -13,6 +13,7 @@ public class MainActivity extends AppCompatActivity {
     setContentView(R.layout.activity_main);
   }
 
+  // TODO try to center button
   public void activateToastButton(View view) {
     CharSequence toastText = "Name: Joviane Bellegarde\nEmail: bellegarde.j@northeastern.edu";
     int duration = Toast.LENGTH_SHORT;
