b4e77a6c5d80715778a68f77c81761676c27a147
jbellegarde <bellegarde.j@northeastern.edu>
2022-02-03 22:22:32 -0500

    reformatted code for secondActivity

--- app/src/main/java/edu/neu/numad22sp_jovianebellegarde/SecondActivity.java
+++ app/src/main/java/edu/neu/numad22sp_jovianebellegarde/SecondActivity.java
@@ -6,13 +6,11 @@ import android.view.View;
 
 public class SecondActivity extends AppCompatActivity {
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_second);
-    }
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_second);
+  }
 
-    public void showButtonMessage(View view) {
-
-    }
+  public void showButtonMessage(View view) {}
 }
