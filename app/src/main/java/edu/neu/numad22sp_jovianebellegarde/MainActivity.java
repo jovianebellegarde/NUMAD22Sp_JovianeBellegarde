c507e9724df348d455cb842527fbe15b47b9b4fc
jbellegarde <bellegarde.j@northeastern.edu>
2022-02-09 23:06:17 -0500

    added TODO in main activity

--- app/src/main/java/edu/neu/numad22sp_jovianebellegarde/MainActivity.java
+++ app/src/main/java/edu/neu/numad22sp_jovianebellegarde/MainActivity.java
@@ -1,5 +1,15 @@
 package edu.neu.numad22sp_jovianebellegarde;
 
+// TODO:
+//  1. new activity needs to have a list of links; will start with 0 items
+//  2. floating action button to start a new link
+//  3. user enters a name and URL for link which is added to the list
+//  4. when user taps a URL in the list, launch url in a web browser
+//  5. use a message in a snackbar to inform user the link was successfully/unsuccessfully created
+//  6. link data has to persist for a configuration change (bundle/parcel stuff)
+//  7. links do not persist if the app is stopped/launched again or if link collector dismissed
+//  8. hit back button to make sure app behaves
+
 import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
@@ -31,5 +41,13 @@ public class MainActivity extends AppCompatActivity {
    */
   public void sixButtonsActivity(View view) {
     startActivity(new Intent(view.getContext(), ButtonActivity.class));
+  }
+
+  /**
+   * Opens a new activity to collect the links.
+   * @param view to get the Context of the message.
+   */
+  public void linkListActivity(View view) {
+    startActivity(new Intent(view.getContext(), LinkCollectorActivity.class));
   }
 }
