53ce3995ea91604ae5094c1b749adcfef1deb7ac
jbellegarde <bellegarde.j@northeastern.edu>
2022-02-13 16:45:11 -0500

    added interface to open link in browser but not sure yet

--- app/src/main/java/edu/neu/numad22sp_jovianebellegarde/LinkClickListener.java
+++ app/src/main/java/edu/neu/numad22sp_jovianebellegarde/LinkClickListener.java
@@ -3,4 +3,6 @@ package edu.neu.numad22sp_jovianebellegarde;
 public interface LinkClickListener {
   // What happens when a link is clicked? a new web page should open up!
   void onLinkClick(int position);
+
+
 }
