7c54336925f0483d29e73a15db1c9e70b8f1822f
jbellegarde <bellegarde.j@northeastern.edu>
2022-02-15 14:15:14 -0500

    changed order of TODO

--- app/src/main/java/edu/neu/numad22sp_jovianebellegarde/LocationActivity.java
+++ app/src/main/java/edu/neu/numad22sp_jovianebellegarde/LocationActivity.java
@@ -5,9 +5,9 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 // TODO Add a new activity - done
+//  Ask for permission only when the user uses this feature, not on installation
 //  Display location (latitude and longitude) from the location sensor
 //  Display these 2 numbers; don't call Google Maps to display the location
-//  Ask for permission only when the user uses this feature, not on installation
 
 public class LocationActivity extends AppCompatActivity {
 
