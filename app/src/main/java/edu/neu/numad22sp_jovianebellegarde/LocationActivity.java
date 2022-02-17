c5040a46655d1fdb0421d6c15e3bfab937015d32
jbellegarde <bellegarde.j@northeastern.edu>
2022-02-17 09:28:56 -0500

    added location button code and need to finish

--- app/src/main/java/edu/neu/numad22sp_jovianebellegarde/LocationActivity.java
+++ app/src/main/java/edu/neu/numad22sp_jovianebellegarde/LocationActivity.java
@@ -1,8 +1,14 @@
 package edu.neu.numad22sp_jovianebellegarde;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+
+import android.Manifest;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 
 // TODO Add a new activity - done
 //  Ask for permission only when the user uses this feature, not on installation
@@ -10,13 +16,15 @@ import android.view.View;
 //  Display these 2 numbers; don't call Google Maps to display the location
 
 public class LocationActivity extends AppCompatActivity {
+  private final Button locationButton = findViewById(R.id.locationButton);
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_location);
   }
 
-  // Aashi suggested to use this class instead: https://developer.android.com/reference/androidx/core/app/ActivityCompat
+  // Aashi suggested to use this class instead of ActivityResultLauncher:
+  // https://developer.android.com/reference/androidx/core/app/ActivityCompat
   public void showLocation(View view) {
     // TODO leaving implementation to Thursday morning
   }
