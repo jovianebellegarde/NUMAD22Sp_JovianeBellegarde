aaac000159d2c9f27014c13530437b86bebf4e88
jbellegarde <bellegarde.j@northeastern.edu>
2022-02-17 09:58:28 -0500

    added activitycompat in oncreate to check if permission exists

--- app/src/main/java/edu/neu/numad22sp_jovianebellegarde/LocationActivity.java
+++ app/src/main/java/edu/neu/numad22sp_jovianebellegarde/LocationActivity.java
@@ -4,11 +4,11 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
 
 import android.Manifest;
-import android.app.AlertDialog;
-import android.content.DialogInterface;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 // TODO Add a new activity - done
 //  Ask for permission only when the user uses this feature, not on installation
@@ -16,16 +16,33 @@ import android.widget.Button;
 //  Display these 2 numbers; don't call Google Maps to display the location
 
 public class LocationActivity extends AppCompatActivity {
-  private final Button locationButton = findViewById(R.id.locationButton);
+
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_location);
+
+    Button locationButton = findViewById(R.id.locationButton);
+
+    // Aashi suggested to use this class instead of ActivityResultLauncher:
+    // https://developer.android.com/reference/androidx/core/app/ActivityCompat
+    locationButton.setOnClickListener(v -> {
+      if (ActivityCompat.checkSelfPermission(v.getContext(), Manifest.permission.ACCESS_FINE_LOCATION)
+              == PackageManager.PERMISSION_GRANTED
+              && ActivityCompat.checkSelfPermission(v.getContext(), Manifest.permission.ACCESS_COARSE_LOCATION)
+              == PackageManager.PERMISSION_GRANTED
+              && ActivityCompat.checkSelfPermission(v.getContext(), Manifest.permission.ACCESS_BACKGROUND_LOCATION)
+              == PackageManager.PERMISSION_GRANTED) {
+         Toast.makeText(LocationActivity.this, "Permission already granted.",
+                 Toast.LENGTH_LONG).show();
+      } else {
+        getLocationPermission(v);
+      }
+    });
   }
 
-  // Aashi suggested to use this class instead of ActivityResultLauncher:
-  // https://developer.android.com/reference/androidx/core/app/ActivityCompat
-  public void showLocation(View view) {
-    // TODO leaving implementation to Thursday morning
+  public void getLocationPermission(View view) {
+    Toast.makeText(LocationActivity.this, "Permission has not been granted yet.",
+            Toast.LENGTH_LONG).show();
   }
 }
