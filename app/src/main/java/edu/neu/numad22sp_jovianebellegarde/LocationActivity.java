de10592b44872ad53298655a4897e4e294692563
jbellegarde <bellegarde.j@northeastern.edu>
2022-02-18 09:44:30 -0500

    got location request to work hopefully

--- app/src/main/java/edu/neu/numad22sp_jovianebellegarde/LocationActivity.java
+++ app/src/main/java/edu/neu/numad22sp_jovianebellegarde/LocationActivity.java
@@ -1,20 +1,17 @@
 package edu.neu.numad22sp_jovianebellegarde;
 
-import androidx.activity.result.ActivityResultLauncher;
-import androidx.activity.result.contract.ActivityResultContracts;
-import androidx.annotation.NonNull;
+import com.google.android.material.snackbar.Snackbar;
+
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
 
 import android.Manifest;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.content.pm.PackageManager;
-import android.location.Location;
-import android.location.LocationListener;
-import android.location.LocationManager;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-import android.widget.TextView;
 import android.widget.Toast;
 
 // TODO Add a new activity - done
@@ -22,8 +19,10 @@ import android.widget.Toast;
 //  Display location (latitude and longitude) from the location sensor
 //  Display these 2 numbers; don't call Google Maps to display the location
 
-public class LocationActivity extends AppCompatActivity {
+public class LocationActivity extends AppCompatActivity
+    implements ActivityCompat.OnRequestPermissionsResultCallback {
   private Button locationButton;
+  private int PERMISSION = 1;
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
@@ -32,12 +31,41 @@ public class LocationActivity extends AppCompatActivity {
     // Aashi suggested to use this class instead of ActivityResultLauncher:
     // Prob only need find location since both latitude and longitude are needed
     // https://developer.android.com/reference/androidx/core/app/ActivityCompat
-    locationButton = findViewById(R.id.getLocationButton);
-    locationButton.setOnClickListener(this::getLocationPermission);
-
+    locationButton = findViewById(R.id.button_get_location);
+    locationButton.setOnClickListener(new View.OnClickListener() {
+      @Override
+      public void onClick(View v) {
+        showLocationPreview();
+      }
+    });
   }
 
-  public void getLocationPermission(View view) {
-    Toast.makeText(this, "getLocationPermission", Toast.LENGTH_LONG).show();
+  public void showLocationPreview() {
+    if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)
+            == PackageManager.PERMISSION_GRANTED) {
+      Toast.makeText(LocationActivity.this, "Permission Granted", Toast.LENGTH_LONG)
+              .show();
+      // call location method
+    } else {
+      requestLocationPermission();
+    }
+  }
+
+  public void requestLocationPermission() {
+    if (ActivityCompat.shouldShowRequestPermissionRationale(this,
+            Manifest.permission.ACCESS_FINE_LOCATION)) {
+      new AlertDialog.Builder(this)
+              .setTitle("Permission Requested")
+              .setMessage("This app is requesting permission to access the device location.")
+              .setPositiveButton("Accept Access", (dialog, which) ->
+                      ActivityCompat.requestPermissions(LocationActivity.this,
+                      new String[] {Manifest.permission.ACCESS_FINE_LOCATION},
+                      PERMISSION)).setNegativeButton("Cancel", (dialog, which) ->
+              dialog.cancel()).create().show();
+    } else {
+      ActivityCompat.requestPermissions(this,
+              new String[] {Manifest.permission.ACCESS_FINE_LOCATION},
+              PERMISSION);
+    }
   }
 }
