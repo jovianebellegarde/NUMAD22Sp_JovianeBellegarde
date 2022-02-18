2a22688c267828b328d28cf00a65f1c18289e47d
jbellegarde <bellegarde.j@northeastern.edu>
2022-02-18 15:49:07 -0500

    permission request now shows up after tapping location button

--- app/src/main/java/edu/neu/numad22sp_jovianebellegarde/LocationActivity.java
+++ app/src/main/java/edu/neu/numad22sp_jovianebellegarde/LocationActivity.java
@@ -7,9 +7,11 @@ import androidx.core.app.ActivityCompat;
 import android.Manifest;
 import android.annotation.SuppressLint;
 import android.app.AlertDialog;
+import android.content.Context;
 import android.content.pm.PackageManager;
 import android.location.Location;
 import android.location.LocationListener;
+import android.location.LocationManager;
 import android.os.Bundle;
 import android.widget.Button;
 import android.widget.TextView;
@@ -23,23 +25,16 @@ import android.widget.Toast;
 public class LocationActivity extends AppCompatActivity implements
         ActivityCompat.OnRequestPermissionsResultCallback, LocationListener {
   private final int PERMISSION = 777;
-  TextView latitudeTextView;
-  TextView longitudeTextView;
+  private TextView latitudeTextView;
+  private TextView longitudeTextView;
+  private LocationManager locationManager;
+  private Location location;
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_location);
-    // Aashi suggested to use this class instead of ActivityResultLauncher:
-    // Prob only need find location since both latitude and longitude are needed
-    // https://developer.android.com/reference/androidx/core/app/ActivityCompat
-    Button locationButton = findViewById(R.id.button_get_location);
-    latitudeTextView = findViewById(R.id.latitude_textview);
-    longitudeTextView = findViewById(R.id.longitude_textview);
-    locationButton.setOnClickListener(v -> activityCompatCheckSelfPermission());
-  }
 
-  public void activityCompatCheckSelfPermission() {
     if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)
             == PackageManager.PERMISSION_GRANTED) {
       Toast.makeText(LocationActivity.this, "Permission Granted", Toast.LENGTH_LONG)
@@ -48,6 +43,12 @@ public class LocationActivity extends AppCompatActivity implements
     } else {
       requestLocationPermission();
     }
+    // locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+    // location = locationManager.getLastKnownLocation(LocationManager.NETWORK_PROVIDER);
+    Button locationButton = findViewById(R.id.button_get_location);
+    latitudeTextView = findViewById(R.id.latitude_textview);
+    longitudeTextView = findViewById(R.id.longitude_textview);
+    locationButton.setOnClickListener(v -> requestLocationPermission());
   }
 
   public void requestLocationPermission() {
@@ -58,10 +59,10 @@ public class LocationActivity extends AppCompatActivity implements
               .setMessage("This app is requesting permission to access the device location.")
               .setPositiveButton("Accept Access", (dialog, which) ->
                       ActivityCompat.requestPermissions(LocationActivity.this,
-                      new String[] {Manifest.permission.ACCESS_FINE_LOCATION},
-                      PERMISSION))
+                              new String[] {Manifest.permission.ACCESS_FINE_LOCATION},
+                              PERMISSION))
               .setNegativeButton("Cancel", (dialog, which) ->
-              dialog.cancel()).create().show();
+                      dialog.cancel()).create().show();
     } else {
       ActivityCompat.requestPermissions(this,
               new String[] {Manifest.permission.ACCESS_FINE_LOCATION},
@@ -69,34 +70,21 @@ public class LocationActivity extends AppCompatActivity implements
     }
   }
 
-  @SuppressLint("MissingSuperCall")
   @Override
   public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions,
                                          @NonNull int[] grantResults) {
-    switch(requestCode) {
-      case PERMISSION:
-        if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-          // call for precise location (latitude and longitude)
-          //Toast.makeText(LocationActivity.this, "switch statement", Toast.LENGTH_SHORT).show();
-          activityResultLauncher();
-        } else {
-          // Explain to the user that the feature is unavailable because
-          // the features requires a permission that the user has denied.
-          // At the same time, respect the user's decision. Don't link to
-          // system settings in an effort to convince the user to change
-          // their decision.
-          new AlertDialog.Builder(this)
-                  .setTitle("Feature Unavailable")
-                  .setMessage("This feature is unavailable because access to location was denied.")
-                  .create().show();
-        }
+    super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+    if (requestCode == PERMISSION) {
+      if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+        Location location = new Location(Manifest.permission.ACCESS_FINE_LOCATION);
+        onLocationChanged(location);
+      } else {
+        new AlertDialog.Builder(this)
+                .setTitle("Feature Unavailable")
+                .setMessage("This feature is unavailable because access to location was denied.")
+                .create().show();
+      }
     }
-  }
-
-  public void activityResultLauncher() {
-    Toast.makeText(LocationActivity.this, "activity result launcher", Toast.LENGTH_SHORT).show();
-    Location location = new Location(Manifest.permission.LOCATION_HARDWARE);
-    onLocationChanged(location);
   }
 
   @SuppressLint("SetTextI18n")
