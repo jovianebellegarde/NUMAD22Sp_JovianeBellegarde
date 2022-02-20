a7d0a69b345d667b93d23f65f1a14f964fd89aa4
jbellegarde <bellegarde.j@northeastern.edu>
2022-02-20 16:01:40 -0500

    ready for submission

--- app/src/main/java/edu/neu/numad22sp_jovianebellegarde/LinkListActivity.java
+++ app/src/main/java/edu/neu/numad22sp_jovianebellegarde/LinkListActivity.java
@@ -6,7 +6,6 @@ import android.os.Bundle;
 
 public class LinkListActivity extends AppCompatActivity {
 
-  // need to setup Alert Dialog in here in onclick
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
