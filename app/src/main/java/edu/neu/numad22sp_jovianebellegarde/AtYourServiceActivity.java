5e2c80e15d0ab51dc38482d665a8dc6cb1f49bf0
jbellegarde <bellegarde.j@northeastern.edu>
2022-03-02 20:27:04 -0500

    added at your service activity

---
+++ app/src/main/java/edu/neu/numad22sp_jovianebellegarde/AtYourServiceActivity.java
@@ -0,0 +1,14 @@
+package edu.neu.numad22sp_jovianebellegarde;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class AtYourServiceActivity extends AppCompatActivity {
+
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_at_your_service);
+  }
+}
