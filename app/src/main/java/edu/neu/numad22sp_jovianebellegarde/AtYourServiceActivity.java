86e26c7e7ff7445570f1a30943704788ed5516fb
jbellegarde <bellegarde.j@northeastern.edu>
2022-03-06 11:04:13 -0500

    updated project gradle and binding in onCreate

--- app/src/main/java/edu/neu/numad22sp_jovianebellegarde/AtYourServiceActivity.java
+++ app/src/main/java/edu/neu/numad22sp_jovianebellegarde/AtYourServiceActivity.java
@@ -1,15 +1,22 @@
 package edu.neu.numad22sp_jovianebellegarde;
 
 import androidx.appcompat.app.AppCompatActivity;
+import edu.neu.numad22sp_jovianebellegarde.databinding.ActivityMainBinding;
 
 import android.os.Bundle;
+import android.view.View;
 
 // TODO - do NOT use volley :(
 public class AtYourServiceActivity extends AppCompatActivity {
 
+  private ActivityMainBinding binding;
+
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
-    setContentView(R.layout.activity_at_your_service);
+    binding = ActivityMainBinding.inflate(getLayoutInflater());
+    View view = binding.getRoot();
+    setContentView(view);
   }
+
 }
