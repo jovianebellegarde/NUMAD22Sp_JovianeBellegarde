477529f5299c8cc3e4bc4d5660a38a0b64ae67a9
jbellegarde <bellegarde.j@northeastern.edu>
2022-03-07 09:58:10 -0500

    added some stuff to the xml file

--- app/src/main/java/edu/neu/numad22sp_jovianebellegarde/AtYourServiceActivity.java
+++ app/src/main/java/edu/neu/numad22sp_jovianebellegarde/AtYourServiceActivity.java
@@ -1,6 +1,7 @@
 package edu.neu.numad22sp_jovianebellegarde;
 
 import android.os.Bundle;
+import android.view.View;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -10,5 +11,8 @@ public class AtYourServiceActivity extends AppCompatActivity {
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_at_your_service);
+  }
+
+  public void getData(View view) {
   }
 }
