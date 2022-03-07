2c137d8badb18adcf7efb23eff67076dc4c1fd3b
jbellegarde <bellegarde.j@northeastern.edu>
2022-03-06 19:43:25 -0500

    starting over from scratch

--- app/src/main/java/edu/neu/numad22sp_jovianebellegarde/AboutMeActivity.java
+++ app/src/main/java/edu/neu/numad22sp_jovianebellegarde/AboutMeActivity.java
@@ -6,9 +6,9 @@ import android.os.Bundle;
 
 public class AboutMeActivity extends AppCompatActivity {
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_about_me);
-    }
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_about_me);
+  }
 }
--- app/src/main/java/edu/neu/numad22sp_jovianebellegarde/AtYourServiceActivity.java
+++ app/src/main/java/edu/neu/numad22sp_jovianebellegarde/AtYourServiceActivity.java
@@ -1,34 +1,18 @@
 package edu.neu.numad22sp_jovianebellegarde;
 
-import androidx.appcompat.app.AppCompatActivity;
-import edu.neu.numad22sp_jovianebellegarde.databinding.ActivityMainBinding;
-
 import android.os.Bundle;
-import android.view.View;
 
-// TODO - do NOT use volley :(
+import androidx.appcompat.app.AppCompatActivity;
+
 public class AtYourServiceActivity extends AppCompatActivity {
-
-  private ActivityMainBinding binding;
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
-    binding = ActivityMainBinding.inflate(getLayoutInflater());
-    View view = binding.getRoot();
-    setContentView(view);
+    setContentView(R.layout.activity_at_your_service);
   }
 
-  public void getData(View view) {
 
-    Runnable runnable = () -> {
-      synchronized (this) {
-        try {
-          // code goes in here to request HTTP
-        } catch (Exception e) {
-          // fill in log errors here
-        }
-      }
-    };
-  }
+
 }
+
