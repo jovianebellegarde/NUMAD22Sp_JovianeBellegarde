5b24c973dd9bbee01bf16e869e366b91a1d16d4e
jbellegarde <bellegarde.j@northeastern.edu>
2022-03-07 11:09:07 -0500

    added empty runnable thread class and handler

--- app/src/main/java/edu/neu/numad22sp_jovianebellegarde/AtYourServiceActivity.java
+++ app/src/main/java/edu/neu/numad22sp_jovianebellegarde/AtYourServiceActivity.java
@@ -1,18 +1,35 @@
 package edu.neu.numad22sp_jovianebellegarde;
 
 import android.os.Bundle;
+import android.os.Handler;
 import android.view.View;
+import android.widget.EditText;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class AtYourServiceActivity extends AppCompatActivity {
 
+  private static final String TAG = "AtYourServiceActivity";
+  private final Handler editTextHandler = new Handler();
+  EditText editText;
+
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_at_your_service);
+    editText = findViewById(R.id.edit_text);
   }
 
+  // runnable thread
   public void getData(View view) {
+    new Thread(new RunnableThread()).start();
+  }
+
+  static class RunnableThread implements Runnable {
+
+    @Override
+    public void run() {
+      //
+    }
   }
 }
