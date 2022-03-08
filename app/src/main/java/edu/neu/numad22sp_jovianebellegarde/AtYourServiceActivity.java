43077aba504f3fbdfc007696940dddf4330ae5c9
jbellegarde <bellegarde.j@northeastern.edu>
2022-03-07 20:29:21 -0500

    added more code to at your service

--- app/src/main/java/edu/neu/numad22sp_jovianebellegarde/AtYourServiceActivity.java
+++ app/src/main/java/edu/neu/numad22sp_jovianebellegarde/AtYourServiceActivity.java
@@ -8,6 +8,10 @@ import android.widget.EditText;
 
 import org.json.JSONObject;
 
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
 import java.net.URL;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -41,11 +45,25 @@ public class AtYourServiceActivity extends AppCompatActivity {
       // try catch block -> https request here
       // user input for choice of json file here
       // send updates back to the main thread
-      JSONObject jsonObject = new JSONObject();
+      // JSONObject jsonObject = new JSONObject();
+
+      StringBuilder stringBuilder = new StringBuilder();
 
       // this is just one hairstyle
       try {
-        URL url = new URL("https://ffxivcollect.com/api/hairstyles/419");
+        URL url = new URL("https://ffxivcollect.com/api/hairstyles");
+        HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+        String string;
+
+        while ((string = bufferedReader.readLine()) != null) {
+          stringBuilder.append(string);
+        }
+        bufferedReader.close();
+
+        if (!stringBuilder.toString().isEmpty()) {
+          // fill in rest here
+        }
       } catch (Exception e) {
         Log.e(TAG, "Exception found");
       }
