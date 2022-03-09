0cb7c72abc923cf709d9b4baceced65e2a386d9f
jbellegarde <bellegarde.j@northeastern.edu>
2022-03-08 22:53:37 -0500

    nothing is working

--- app/src/main/java/edu/neu/numad22sp_jovianebellegarde/AtYourServiceActivity.java
+++ app/src/main/java/edu/neu/numad22sp_jovianebellegarde/AtYourServiceActivity.java
@@ -1,26 +1,36 @@
 package edu.neu.numad22sp_jovianebellegarde;
 
+import androidx.appcompat.app.AppCompatActivity;
+import edu.neu.numad22sp_jovianebellegarde.databinding.ActivityAtYourServiceBinding;
+
+import android.annotation.SuppressLint;
 import android.os.Bundle;
 import android.os.Handler;
 import android.util.Log;
 import android.view.View;
-import android.widget.EditText;
 
+import org.json.JSONArray;
+import org.json.JSONException;
 import org.json.JSONObject;
 
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
+import java.io.IOException;
+import java.io.InputStream;
 import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.ProtocolException;
 import java.net.URL;
-
-import androidx.appcompat.app.AppCompatActivity;
-import edu.neu.numad22sp_jovianebellegarde.databinding.ActivityAtYourServiceBinding;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
 
 public class AtYourServiceActivity extends AppCompatActivity {
 
   private static final String TAG = "AtYourServiceActivity";
-  private ActivityAtYourServiceBinding binding;
-  private final Handler editTextHandler = new Handler();
+
+  @SuppressLint("StaticFieldLeak")
+  private static ActivityAtYourServiceBinding binding;
+
+  public static final Handler handler = new Handler();
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
@@ -36,53 +46,98 @@ public class AtYourServiceActivity extends AppCompatActivity {
   }
 
   static class RunnableThread implements Runnable {
+    List<String> hairList = new ArrayList();
 
     @Override
     public void run() {
-      // try catch block -> https request here
-      // user input for choice of json file here
-      // send updates back to the main thread
-      // JSONObject jsonObject = new JSONObject();
-
-      StringBuilder stringBuilder = new StringBuilder();
-
-      // this is just one hairstyle
       try {
         URL url = new URL("https://ffxivcollect.com/api/hairstyles");
         HttpURLConnection connection = (HttpURLConnection) url.openConnection();
-        BufferedReader bufferedReader =
-            new BufferedReader(new InputStreamReader(connection.getInputStream()));
-        String string;
+        connection.setRequestMethod("GET");
+        connection.setDoInput(true);
+        connection.connect();
 
-        while ((string = bufferedReader.readLine()) != null) {
-          stringBuilder.append(string);
-        }
-        bufferedReader.close();
+        InputStream inputStream = connection.getInputStream();
+        final String response = convertTheStringIntoStream(inputStream);
 
-        if (!stringBuilder.toString().isEmpty()) {
-          JSONObject jsonObject = new JSONObject(stringBuilder.toString());
+        //JSONObject jsonObject = new JSONObject(response);
+        //JSONArray jsonArrayOfObjects = jsonObject.getJSONArray("results");
+        //JSONArray jsonArray = new JSONArray(response);
+
+        JSONArray results = new JSONArray(response);
+        for (int i = 0; i < results.length(); i++) {
+          JSONObject result = results.getJSONObject(i);
+          String name = result.getString("name");
+          String description = result.getString("description");
+          if (name.equals(binding.editText.getText().toString())) {
+            handler.post(() -> binding.textView.setText(description));
+          }
         }
-      } catch (Exception e) {
-        Log.e(TAG, "Exception found");
+        // loop through the array of objects -> for each object, need to find the name specified
+        // return the description
+//        for (int i = 0; i < jsonArrayOfObjects.length(); i++) {
+//          if (jsonArrayOfObjects.get(i) == binding.editText.getText()) {
+//            binding.textView.setText("Wind Caller");
+//          }
+//        }
+        // System.out.println(jsonObject.toString() + "Json Object");
+        // String name = jsonObject.getString("name");
+        //String description = jsonObject.getString("description");
+        // hairList.add(name);
+        // hairList.add(description);
+        //handler.post(() -> binding.textView.setText(hairList.get(0)));
+      } catch (MalformedURLException e) {
+        Log.e(TAG, "MalformedURLException");
+        e.printStackTrace();
+      } catch (ProtocolException e) {
+        Log.e(TAG, "ProtocolException");
+        e.printStackTrace();
+      } catch (IOException e) {
+        Log.e(TAG, "IOException");
+        e.printStackTrace();
+      } catch (JSONException e) {
+        Log.e(TAG, "JSONException");
+        e.printStackTrace();
       }
+
+      //      // try catch block -> https request here
+      //      // user input for choice of json file here
+      //      // send updates back to the main thread
+      //      // JSONObject jsonObject = new JSONObject();
+      //
+      //      StringBuilder stringBuilder = new StringBuilder();
+      //
+      //      // this is just one hairstyle
+      //      try {
+      //
+      //        URL url = new URL("https://ffxivcollect.com/api/hairstyles");
+      //        HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+      //        BufferedReader bufferedReader =
+      //            new BufferedReader(new InputStreamReader(connection.getInputStream()));
+      //        String string;
+      //
+      //        while ((string = bufferedReader.readLine()) != null) {
+      //          stringBuilder.append(string);
+      //        }
+      //        bufferedReader.close();
+      //
+      //        if (!stringBuilder.toString().isEmpty()) {
+      //          JSONObject jsonObject = new JSONObject(stringBuilder.toString());
+      //        }
+      //      } catch (Exception e) {
+      //        Log.e(TAG, "Exception found");
+      //      }
+    }
+
+    private String convertTheStringIntoStream(InputStream inputStream) {
+      Scanner scanner = new Scanner(inputStream).useDelimiter("\\A");
+      String returnString;
+      if (scanner.hasNext()) {
+        returnString = scanner.next().replace(",", ",\n");
+      } else {
+        returnString = "";
+      }
+      return returnString;
     }
   }
 }
-
-// import androidx.appcompat.app.AppCompatActivity;
-//
-//
-//
-// import android.os.Bundle;
-//
-//
-//
-// public class AtYourServiceActivity extends AppCompatActivity {
-//
-//
-//  @Override
-//  protected void onCreate(Bundle savedInstanceState) {
-//    super.onCreate(savedInstanceState);
-//    setContentView(R.layout.activity_at_your_service);
-//  }
-// }
