41e65902be53033bc7b819c0cd30ba1196e85d7d
jbellegarde <bellegarde.j@northeastern.edu>
2022-04-07 21:57:25 -0400

    added TODO

--- app/src/main/java/edu/neu/numad22sp_jovianebellegarde/LinkListActivity.java
+++ app/src/main/java/edu/neu/numad22sp_jovianebellegarde/LinkListActivity.java
@@ -6,6 +6,7 @@ import android.os.Bundle;
 
 public class LinkListActivity extends AppCompatActivity {
 
+  // TODO take another look at this
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
